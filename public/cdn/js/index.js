dab33d391411253e6ae17a4cf995702425e0c065
Nityanand Thakur <nityanandthakurdss@gmail.com>
2021-10-04 19:41:16 +0530

    `initial commit`

---
+++ public/cdn/js/index.js
@@ -0,0 +1,74 @@
+function submit() {
+  let x = document.getElementById("id12").value;
+  document.getElementById("output121").innerHTML = x;
+  document.getElementById("out1").innerHTML = "Thanks for your response";
+}
+
+const port = "nnt";
+// function nnt() {
+//   document.getElementById("out").innerHTML = y.name + " is my name";
+// }
+
+x = {
+  name: "Nityanand Thakur",
+  class: "X A",
+  roll: "14",
+  // "age": "14",
+};
+
+
+y = JSON.constructor(x);
+const a212 =
+  y.name +
+  " is my name " +
+  "and my age is " +
+  y.roll +
+  " and my class is " +
+  y.class +
+  " and my roll no. is " +
+  y.roll +
+  " .";
+  console.log(a212);
+  console.warn(a212);
+  console.error(a212);
+function ctof() {
+  let x = document.getElementById("777").value;
+  const y = x * 1.8 + 32;
+  str(y);
+}
+function str(x) {
+  document.getElementById("ctof1").innerHTML = x;
+}
+function ftoc() {
+  let x = document.getElementById("77").value;
+  const y1 = (x - 32) / 1.8;
+  document.getElementById("ftoc").innerHTML = y1;
+}
+function atosin() {
+  let x = document.getElementById("7777").value;
+  let x1 = x * 0.0174532925199433;
+  const y3 = Math.sin(x1);
+  document.getElementById("atosin").innerHTML = y3;
+}
+function atocos() {
+  let x = document.getElementById("7").value;
+  let x1 = x * 0.0174532925199433;
+  const y4 = Math.cos(x1);
+  document.getElementById("atocos").innerHTML = y4;
+}
+function atotan() {
+  let x = document.getElementById("77777").value;
+  let x1 = x * 0.0174532925199433;
+  const y5 = Math.tan(x1);
+  document.getElementById("atotan").innerHTML = y5;
+}
+function atorad() {
+  let x = document.getElementById("777777").value;
+  let x1 = x * 0.0174532925199433;
+  document.getElementById("atorad").innerHTML = x1;
+}
+function radtoa() {
+  let x = document.getElementById("7777777").value;
+  let x1 = x * 57.2957795130823;
+  document.getElementById("radtoa").innerHTML = x1;
+}
